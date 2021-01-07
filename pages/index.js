a0127f0c9835fc5c1689bc3b7eae021aabc2b098
Miguel Rosa <miguelg1@motorola.com>
2021-01-07 18:28:29 -0300

    add search console tag

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Summer Hill Modas</title>
+        <meta name="google-site-verification" content="M3dM7HGFRCGYVdDEHKOirldW57YgFi63GJR5hIFBmFo" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet" />
