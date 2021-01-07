f4bc474966e915b5a45f08a9f15d459a07adb3e2
Miguel Rosa <miguelg1@motorola.com>
2021-01-07 16:51:21 -0300

    fix mobile header, and add social media links

--- components/Product.js
+++ components/Product.js
@@ -7,7 +7,7 @@ import { FaWhatsapp as WhatsappIcon} from 'react-icons/fa';
 const Product = ({data}) => {
     
     return (
-        <a href={`${data.url}?text="Olá, me interessei ${data.product ? 'pelo '+ data.product : 'por um produto no site'}, como posso compra-lo?"`} className={styles.product} style={{backgroundImage:`url(${data.image})`}}>
+        <a href={`${data.url}?text=Olá, me interessei ${data.product ? 'pelo '+ data.product : 'por um produto no site'}, como posso compra-lo?`} className={styles.product} style={{backgroundImage:`url(${data.image})`}}>
             <div className={styles.button}><WhatsappIcon className={styles.icon}/>Comprar</div>
         </a>
     )
