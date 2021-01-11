370aeace9b563cb4ea62fa6508977a9b75252eda
Miguel Rosa <miguelg1@motorola.com>
2021-01-11 19:26:58 -0300

    fix production api url

--- services/api.js
+++ services/api.js
@@ -1,5 +1,5 @@
 const PRODUCTION = true;
-const API_URL = PRODUCTION ? "http://store-api-miguel.herokuapp.com/" : "http://localhost:1337";
+const API_URL = PRODUCTION ? "http://store-api-miguel.herokuapp.com" : "http://localhost:1337";
 
 export function STORE_GET(slug){
     return {
